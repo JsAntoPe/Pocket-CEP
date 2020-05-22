b3da770887cc8a9ef1cd5680fea3e381ba94976e
JsAntoPe <jsperegrina1@gmail.com>
2020-05-22 14:16:18 +0200

    Custom event entity created. It can receive events as normal jsons, instead of relying on a json with a single attribute with the entire event as a stream

---
+++ src/main/java/com/siddhiApi/entity/CustomEvent.java
@@ -0,0 +1,19 @@
+package com.siddhiApi.entity;
+
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+
+public class CustomEvent {
+    Logger logger = LoggerFactory.getLogger(CustomEvent.class);
+    Map<String, Object> eventParameters = new LinkedHashMap<>();
+
+    @JsonAnySetter
+    void setEventParameters(String parameter, Object value){
+        eventParameters.put(parameter, value);
+    }
+}
