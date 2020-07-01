5d613be7152b319092e80052c28bd0fd72939751
JsAntoPe <jsperegrina1@gmail.com>
2020-07-01 12:48:43 +0200

    Name changed and added logger.

--- src/main/java/com/siddhiApi/services/ApplicationServiceImpl.java
+++ src/main/java/com/siddhiApi/services/ApplicationServiceImpl.java
@@ -8,15 +8,14 @@ import com.siddhiApi.util.CustomEventToObjectArray;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class applicationServiceImpl implements applicationService{
+public class ApplicationServiceImpl implements ApplicationService {
 
-    private Logger logger = LoggerFactory.getLogger(applicationServiceImpl.class);
+    private Logger logger = LoggerFactory.getLogger(ApplicationServiceImpl.class);
 
     @Autowired
     private SiddhiDAO siddhiDAO;
