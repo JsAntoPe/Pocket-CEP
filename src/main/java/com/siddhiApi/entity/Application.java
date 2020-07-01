f302a10fcfd1cb12ea48ceedc67a92546a288f05
JsAntoPe <jsperegrina1@gmail.com>
2020-07-01 12:49:16 +0200

    Changed how parameters are sorted in constructor.

--- src/main/java/com/siddhiApi/entity/Application.java
+++ src/main/java/com/siddhiApi/entity/Application.java
@@ -1,20 +1,24 @@
 package com.siddhiApi.entity;
 
+import java.util.List;
+
 public class Application {
-private String inputStreamName, outputStreamName, applicationCode;
-	
-	public Application(String inputStreamName, String outputStreamName, String applicationCode) {
-		this.inputStreamName = inputStreamName;
+	private String applicationName, outputStreamName, applicationCode;
+	private List<String> inputStreamNames;
+
+	public Application(String applicationName, List<String> inputStreamNames, String outputStreamName, String applicationCode) {
+		this.applicationName = applicationName;
+		this.inputStreamNames = inputStreamNames;
 		this.outputStreamName = outputStreamName;
 		this.applicationCode = applicationCode;
 	}
 
-	public String getInputStreamName() {
-		return inputStreamName;
+	public String getApplicationName() {
+		return applicationName;
 	}
 
-	public void setInputStreamName(String inputStreamName) {
-		this.inputStreamName = inputStreamName;
+	public void setApplicationName(String applicationName) {
+		this.applicationName = applicationName;
 	}
 
 	public String getOutputStreamName() {
@@ -31,5 +35,13 @@ private String inputStreamName, outputStreamName, applicationCode;
 
 	public void setApplicationCode(String applicationCode) {
 		this.applicationCode = applicationCode;
+	}
+
+	public List<String> getInputStreamNames() {
+		return inputStreamNames;
+	}
+
+	public void setInputStreamNames(List<String> inputStreamNames) {
+		this.inputStreamNames = inputStreamNames;
 	}
 }
