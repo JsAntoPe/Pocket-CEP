72ea843eddaaad8ade3bafb5fa83cd98154e6b8d
JsAntoPe <jsperegrina1@gmail.com>
2020-07-02 21:42:10 +0200

    Corrected the send event operation, so it parses the values for the correct stream

--- src/main/java/com/siddhiApi/services/StreamServiceImpl.java
+++ src/main/java/com/siddhiApi/services/StreamServiceImpl.java
@@ -2,8 +2,12 @@ package com.siddhiApi.services;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.siddhiApi.dao.SiddhiDAO;
+import com.siddhiApi.dao.SiddhiDAOImpl;
+import com.siddhiApi.dao.StreamDAOImpl;
 import com.siddhiApi.entity.Stream;
 import com.siddhiApi.dao.StreamDAO;
+import com.siddhiApi.util.Parsers;
 import org.everit.json.schema.ValidationException;
 import org.json.JSONObject;
 import org.everit.json.schema.Schema;
@@ -21,26 +25,35 @@ public class StreamServiceImpl implements StreamService{
     Logger logger = LoggerFactory.getLogger(StreamServiceImpl.class);
 
     @Autowired
-    private StreamDAO streamDao;
+    private final StreamDAO streamDao = new StreamDAOImpl();
+
+    @Autowired
+    private final SiddhiDAO siddhiDAO = new SiddhiDAOImpl();
 
     @Override
     public void createStream(Stream stream) {
-        streamDao.createSchema(stream);
+        streamDao.createStream(stream);
     }
 
     @Override
     public Stream getStream(String stream) {
-        return streamDao.getSchema(stream);
+        return streamDao.getStream(stream);
     }
 
     @Override
     public void sendEvent(String stream, Object event) throws ValidationException, JsonProcessingException {
-        JSONObject streamSchema = streamDao.getSchema(stream).getJsonSchema();
+        JSONObject streamSchema = streamDao.getStream(stream).getJsonSchema();
 
         ObjectMapper mapper = new ObjectMapper();
         JSONObject eventSchema = new JSONObject(mapper.writeValueAsString(event));
 
         Schema schema = SchemaLoader.load(streamSchema);
         schema.validate(eventSchema);
+
+        Object[] eventParsed = Parsers.JsonObjectToObjectArray(eventSchema);
+        for (Object object: eventParsed){
+            logger.info("Property on event already parsed: " + object);
+        }
+        siddhiDAO.sendEvent(stream, eventParsed);
     }
 }
