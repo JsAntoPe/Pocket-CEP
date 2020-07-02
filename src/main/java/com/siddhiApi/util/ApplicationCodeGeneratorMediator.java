c32113e2479403fc81cdf2f87cab456616c86b29
JsAntoPe <jsperegrina1@gmail.com>
2020-07-02 21:08:03 +0200

    Added the generation code for streams, as a mediator to encapsulate the communication. The latter will allow to increase the number of generators in a future.

--- src/main/java/com/siddhiApi/util/ApplicationCodeGeneratorMediator.java
+++ src/main/java/com/siddhiApi/util/ApplicationCodeGeneratorMediator.java
@@ -1,4 +1,30 @@
 package com.siddhiApi.util;
 
+import com.siddhiApi.entity.Application;
+import com.siddhiApi.services.StreamService;
+import com.siddhiApi.services.StreamServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ApplicationCodeGeneratorMediator {
+
+    static Logger logger = LoggerFactory.getLogger(ApplicationCodeGeneratorMediator.class);
+
+    private static final StreamService streamService = new StreamServiceImpl();
+
+    private static final StreamGenerator streamGenerator = new StreamGeneratorSiddhi();
+
+    public static String getFullApplicationCode(Application application){
+        String applicationCode = "";
+        for(String inputStreamName: application.getInputStreamNames()){
+            logger.info("InputStreamName: " + inputStreamName);
+            logger.info("InputStreamName from stream itself: " + streamService.getStream(inputStreamName).getStreamID());
+            applicationCode += streamGenerator.generateCode(streamService.getStream(inputStreamName));
+        }
+        applicationCode += application.getApplicationCode();
+        //applicationCode += streamGenerator.generateCode(streamService.getStream(application.getOutputStreamName()));
+
+        logger.info("Application code: " + applicationCode);
+        return applicationCode;
+    }
 }
