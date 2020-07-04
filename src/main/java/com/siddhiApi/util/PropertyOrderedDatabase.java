f6948d94d96684d98fc838be57b43d91301b6655
JsAntoPe <jsperegrina1@gmail.com>
2020-07-04 18:21:32 +0200

    In memory database, so we can save the PropertyFirstAppearance instances.

--- src/main/java/com/siddhiApi/util/PropertyOrderedDatabase.java
+++ src/main/java/com/siddhiApi/util/PropertyOrderedDatabase.java
@@ -1,7 +1,28 @@
 package com.siddhiApi.util;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class PropertyOrderedDatabase {
-    private Map<String, String[]>
+    private Map<String, String[]> outputStreamPropertiesOrdered;
+    private static PropertyOrderedDatabase propertyOrderedDatabase;
+
+    private PropertyOrderedDatabase() {
+        this.outputStreamPropertiesOrdered = new HashMap<>();
+    }
+
+    public static PropertyOrderedDatabase getPropertyOrderedDatabase() {
+        if(propertyOrderedDatabase == null){
+            propertyOrderedDatabase = new PropertyOrderedDatabase();
+        }
+        return propertyOrderedDatabase;
+    }
+
+    public void addOutputStreamPropertiesOrdered(String outputStreamName, String[] propertiesOrdered){
+        outputStreamPropertiesOrdered.put(outputStreamName, propertiesOrdered);
+    }
+
+    public void removeOutputStreamPropertiesOrdered(String outputStreamName){
+        outputStreamPropertiesOrdered.remove(outputStreamName);
+    }
 }
