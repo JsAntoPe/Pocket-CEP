b3608ffe81be77d03034da0e4c3d2a6c41d7704a
JsAntoPe <jsperegrina1@gmail.com>
2020-07-05 23:47:11 +0200

    Added the webhook class, the one that will execute the http operations, and webhook mediator, to provide the operations for creating the former objects.

--- src/main/java/com/siddhiApi/webhook/Webhook.java
+++ src/main/java/com/siddhiApi/webhook/Webhook.java
@@ -1,4 +1,77 @@
 package com.siddhiApi.webhook;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Arrays;
+
 public class Webhook{
+    private final URL url;
+    private final String method;
+    private final String body;
+
+
+
+    Logger logger = LoggerFactory.getLogger(Webhook.class);
+
+    public Webhook(String url, String method, String body) throws MalformedURLException {
+        this.url = new URL(url);
+        this.method = method;
+        this.body = body;
+        /*Thread thread = new Thread(() -> {
+            try {
+                HttpURLConnection connection = (HttpURLConnection) this.url.openConnection();
+                connection.setRequestMethod(this.method);
+                connection.setRequestProperty("Content-Type", "application/json; utf-8");
+                connection.setDoOutput(true);
+                try(OutputStream os = connection.getOutputStream()) {
+                    byte[] input = this.body.getBytes("utf-8");
+                    os.write(input, 0, input.length);
+                }
+                logger.info("Connection Request method: " +connection.getRequestMethod());
+                logger.info("Connection Content type: " +connection.getRequestProperty("Content type"));
+                logger.info("Connection Do output: " +connection.getDoOutput());
+                connection.connect();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });*/
+        try {
+            HttpURLConnection connection = (HttpURLConnection) this.url.openConnection();
+            connection.setRequestMethod(this.method);
+            connection.setRequestProperty("Content-Type", "application/json; utf-8");
+            connection.setDoOutput(true);
+            try(OutputStream os = connection.getOutputStream()) {
+                byte[] input = this.body.getBytes("utf-8");
+                os.write(input, 0, input.length);
+            }
+            logger.info("Connection Request method: " +connection.getRequestMethod());
+            logger.info("Connection Content type: " +connection.getRequestProperty("Content-Type"));
+            logger.info("Connection Do output: " +connection.getDoOutput());
+            connection.connect();
+            try(BufferedReader br = new BufferedReader(
+                    new InputStreamReader(connection.getInputStream(), "utf-8"))) {
+                StringBuilder response = new StringBuilder();
+                String responseLine = null;
+                while ((responseLine = br.readLine()) != null) {
+                    response.append(responseLine.trim());
+                }
+                logger.info(response.toString());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        logger.info("URL: " + this.url);
+        logger.info("Method: " + this.method);
+        logger.info("Body: " + this.body);
+
+        //thread.start();
+    }
 }
--- src/main/java/com/siddhiApi/webhook/WebhookMediator.java
+++ src/main/java/com/siddhiApi/webhook/WebhookMediator.java
@@ -1,4 +1,80 @@
 package com.siddhiApi.webhook;
 
+import com.siddhiApi.entity.Stream;
+import com.siddhiApi.entity.Subscription;
+import com.siddhiApi.services.StreamService;
+import com.siddhiApi.services.StreamServiceImpl;
+import com.siddhiApi.util.PropertyOrderedDatabase;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.MalformedURLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class WebhookMediator {
+    static Logger logger = LoggerFactory.getLogger(WebhookMediator.class);
+
+    private static StreamService streamService = new StreamServiceImpl();
+    private static PropertyOrderedDatabase propertyOrderedDatabase = PropertyOrderedDatabase.getPropertyOrderedDatabase();
+
+    public static void webhookFromSiddhiApp(String streamName, Object[] data){
+        String dataToJSON = dataToJSON(streamName, data);
+        try {
+            new Webhook("http://localhost:8080/api/v1/streams/" + streamName + "/events", "POST", dataToJSON);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void webhookFromSubscription(List<Subscription> subscriptions, Object event){
+        for (Subscription subscription: subscriptions){
+            try {
+                new Webhook(subscription.getWebhook(), subscription.getMethod(), event.toString());
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static Map<String, String> jsonPropertyToSiddhiProperty= new HashMap<String, String>(){
+        {
+            put("number", "double");
+            put("string", "string");
+            put("integer", "int");
+            put("boolean", "boolean");
+        }
+    };
+
+    private static String dataToJSON(String streamName, Object[] data){
+        String json = "{";
+        Stream stream = streamService.getStream(streamName);
+        JSONObject schemaToStream = new JSONObject(stream.getJsonSchema().get("properties").toString());
+
+        Map<String, String> mapPropertyType = new HashMap<>();
+        for(String key : schemaToStream.keySet()){
+            String[] parts = schemaToStream.get(key).toString().split("\"");
+            mapPropertyType.put(key, jsonPropertyToSiddhiProperty.get(parts[3]));
+        }
+        String[] propertiesOrdered = propertyOrderedDatabase.getPropertiesOrdered(streamName);
+        logger.info("Properties Ordered second property: " + propertiesOrdered[1]);
+        for (int i = 0; i < propertiesOrdered.length; ++i){
+            json +=  "\"" + propertiesOrdered[i] + "\": ";
+            logger.info("Map property: " + mapPropertyType.get(propertiesOrdered[i]));
+            if (mapPropertyType.get(propertiesOrdered[i]).equals("string")){
+                json += "\"" + data[i].toString() + "\"";
+            }
+            else{
+                json += data[i].toString();
+            }
+            if(i != propertiesOrdered.length - 1){
+                json += ",";
+            }
+        }
+        json += "}";
+        logger.info("JSON created: " + json);
+        return json;
+    }
 }
