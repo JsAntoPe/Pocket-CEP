7f8df9d37483749e56aea436033bb8e30a601e94
JsAntoPe <jsperegrina1@gmail.com>
2020-07-21 18:03:10 +0200

    OAS3 customized for Pocket-CEP.

---
+++ src/main/java/com/siddhiApi/oas3/OpenApiConfig.java
@@ -0,0 +1,18 @@
+package com.siddhiApi.oas3;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info().title("Pocket-CEP").description(
+                        "This is the OAS3 for Pocket-CEP."));
+    }
+}
