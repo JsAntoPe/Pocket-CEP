0083b327699cac3eeb45f3fd85ad4ffed54d9440
JsAntoPe <jsperegrina1@gmail.com>
2020-07-22 11:24:35 +0200

    Removing hello operation.

--- src/main/java/com/siddhiApi/ApiRestApplication.java
+++ src/main/java/com/siddhiApi/ApiRestApplication.java
@@ -6,6 +6,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
